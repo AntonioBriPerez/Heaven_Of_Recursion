ad09185c9589270a3867c100825f8f92a8894365
AntonioBriPerez <abp81@alu.ua.es>
2021-11-16 18:18:50 +0100

    aritmetic operations expressed as recursive

---
+++ basic_aritmetic_operations_as_recursive.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+//return x + 1
+int suc(int x) {
+    return x + 1;
+}
+
+
+//return x+y
+int suma(int x, int y){
+    if (x == 0) return y;
+    if (y == 0) return x;
+    else{
+        return suc(suma(x, y - 1));
+    }
+}
+
+//return x*y
+int mult(int x, int y){
+    if (x == 0   || y == 0) return 0;
+    if (x== 1) return y;
+    if (y== 1) return x;
+    else {
+        return x + mult(x, y - 1);
+    }
+}
+
+// return x^y
+int exp(int x, int y) {
+    if(x==0) return 0;
+    if(y==0) return 1;
+    else{
+        return mult(x,  exp(x, y - 1));
+    }
+}
+int main(){
+    cout << exp(2, 4) << endl;
+    cout << mult(3, 2) << endl;
+    cout << suma(5, 7) << endl;
+}
